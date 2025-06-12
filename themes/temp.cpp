b90eba3d5411caab0ed2e9bf4f92f49041e64ec4
Chandan <chandansahoo02468@gmail.com>
2025-06-12 18:25:42 +0530

    Added dark and light theme

---
+++ themes/temp.cpp
@@ -0,0 +1,21 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+
+void realmsDomain(){
+    
+}
+
+int main() {
+    velociraptor
+    ll tsts = 1 ; cin>>tsts;
+    string hello = "Chandan";
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+    return 0;
+}
